1c63259a1b53558639afa6cea4ca11d039b86952
Christian de Botton <cdebotton@gmail.com>
2018-06-04 12:56:45 -0400

    Remove postgraphile and pg

--- db/down/1528060889632-createTimestampFunc.sql
+++
@@ -1,4 +0,0 @@
--- Generated migration 1528060889632-createTimestampFunc.sql
-
-DROP FUNCTION IF EXISTS cmyk.trigger_set_timestamp();
-DROP ROLE IF EXISTS admin;
--- db/down/1528065613660-createAuthenticate.sql
+++
@@ -1,4 +0,0 @@
--- Generated migration 1528065613660-createAuthenticate.sql
-
-DROP FUNCTION cmyk.authenticate;
-DROP TYPE cmyk.jwt_token;
