5c69c0720e72cb82986cb52fe0a2888be966cafb
Christian de Botton <cdebotton@gmail.com>
2018-09-04 22:05:25 -0400

    Add watcher for generating types

---
+++ src/app/__generated__/Users.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Users
+// ====================================================
+
+export interface Users_users {
+  __typename: 'User';
+  id: string;
+}
+
+export interface Users {
+  users: Users_users[];
+}
