96cc8b9a5affb6d2837a8e3f07c9038d80c8e1a6
Christian de Botton <cdebotton@gmail.com>
2018-09-07 02:15:10 -0400

    Begin edit user form

--- src/app/AdminEditUser.tsx
+++ src/app/AdminEditUser.tsx
@@ -1,15 +1,72 @@
+import { Field, Formik } from 'formik';
+import gql from 'graphql-tag';
 import React from 'react';
+import { Query } from 'react-apollo';
 import { hot } from 'react-hot-loader';
 import { RouteComponentProps } from 'react-router';
 import styled from 'styled-components';
 import Heading from './components/Heading';
 
-interface IProps extends RouteComponentProps<{ userId: string }> {}
+const USER_QUERY = gql`
+  query AdminEditUser($where: UserWhereUniqueInput!) {
+    user(where: $where) {
+      id
+      email
+      profile {
+        firstName
+        lastName
+      }
+    }
+  }
+`;
 
-function AdminEditUser({ match }: IProps) {
+interface IValues {
+  email: string;
+}
+
+interface IProps extends RouteComponentProps<{ userId: string }> {
+  className?: string;
+}
+
+function AdminEditUser({ className, match }: IProps) {
   return (
-    <div>
-      <Heading>Edit User {match.params.userId}</Heading>
+    <div className={className}>
+      <Query
+        query={USER_QUERY}
+        variables={{ where: { id: match.params.userId } }}
+      >
+        {({ data, loading, error }) => {
+          if (error) {
+            return null;
+          }
+
+          if (loading || !data) {
+            return null;
+          }
+
+          return (
+            <>
+              <Heading>Edit User {data.user.email}</Heading>
+              <Formik<IValues>
+                onSubmit={values => {
+                  // tslint:disable-next-line no-console
+                  console.log(values);
+                }}
+                initialValues={{
+                  email: data.user.email,
+                }}
+              >
+                {({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Field name="email" value={data.user.email} />
+                    <button type="submit">Save</button>
+                  </form>
+                )}
+              </Formik>
+            </>
+          );
+        }}
+      </Query>
     </div>
   );
 }
