bd0b76cbbe9f461346115c4df7089fe024e4cc8b
Christian de Botton <cdebotton@gmail.com>
2018-10-22 23:41:09 -0400

    Work on input animations

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,15 +1,9 @@
 import { FieldProps } from 'formik';
 import { modularScale, position, rem, size } from 'polished';
-import React, { CSSProperties, HTMLProps } from 'react';
+import React, { CSSProperties, HTMLProps, ReactNode } from 'react';
 import { animated, Spring } from 'react-spring';
 import styled from 'styled-components';
 import Toggle from '../containers/Toggle';
-
-interface Props extends FieldProps<any> {
-  className?: string;
-  label: string;
-  type?: HTMLProps<HTMLInputElement>['type'];
-}
 
 const InputField = styled.input`
   position: relative;
@@ -17,19 +11,14 @@ const InputField = styled.input`
   height: ${rem(32)};
   width: 100%;
   border: none;
-  background-color: transparent;
-  color: #fff;
-  &:focus {
-    outline: none;
-  }
-`;
-
-const InputWrapper = styled.span`
-  position: relative;
+  color: #000;
   overflow: hidden;
   display: block;
   border-radius: 3px;
-  background-color: hsla(0, 0%, 100%, 0.2);
+  background-color: #fff;
+  &:focus {
+    outline: none;
+  }
 `;
 
 const Notice = styled.span`
@@ -40,58 +29,129 @@ const Notice = styled.span`
 `;
 
 const InputContainer = styled.span`
+  transform-style: preserve-3d;
+  perspective: 800;
   z-index: 0;
   position: relative;
   padding: ${rem(16)} 0 ${rem(8)};
 `;
 
-const labelStyles: CSSProperties = {
-  left: rem(8),
-  pointerEvents: 'none',
-  position: 'absolute',
-  top: 0,
-};
-
 const PLACEHOLDER: CSSProperties = {
-  ...labelStyles,
-  color: '#eee',
+  color: '#333',
   fontSize: modularScale(0),
-  transform: `translate3d(0, ${rem(24)}, 0)`,
+  transform: `translate3d(0, ${rem(24)}, ${rem(0)})`,
 };
 
 const LABEL: CSSProperties = {
-  ...labelStyles,
   color: '#fff',
   fontSize: modularScale(-1),
-  transform: `translate3d(0, ${rem(3)}, 0)`,
+  transform: `translate3d(0, ${rem(0)}, ${rem(0)})`,
 };
 
-function getHoverStyles(options: { hover: boolean; focus: boolean }) {
-  const { hover } = options;
-  const baseStyles: CSSProperties = {
-    backgroundColor: 'hsla(212, 50%, 50%, 0)',
-    height: '100%',
-    left: 0,
-    position: 'absolute',
-    top: 0,
-    transform: `translate3d(100%, 0, 0) skew(-30deg)`,
-    width: 'calc(100%)',
-  };
+const FOCUSED_LABEL: CSSProperties = {
+  color: '#fff',
+  fontSize: modularScale(-1),
+  transform: `translate3d(0, ${rem(0)}, ${rem(15)})`,
+};
 
-  if (hover) {
-    return {
-      ...baseStyles,
-      backgroundColor: 'hsla(240, 50%, 50%, 0.5)',
-      transform: `translate3d(16px, 0, 0) skew(45deg)`,
-    };
+function ReactiveLabel(props: {
+  empty: boolean;
+  focused: boolean;
+  htmlFor: string;
+  children: ReactNode;
+}) {
+  const { empty, children, focused, htmlFor } = props;
+  return (
+    <Spring
+      native
+      to={empty ? PLACEHOLDER : focused ? FOCUSED_LABEL : LABEL}
+      render={styles => (
+        <animated.label
+          htmlFor={htmlFor}
+          style={{
+            ...styles,
+            cursor: 'pointer',
+            left: rem(8),
+            pointerEvents: empty ? 'none' : 'inherit',
+            position: 'absolute',
+            top: 0,
+          }}
+        >
+          {children}
+        </animated.label>
+      )}
+    />
+  );
+}
+
+enum BorderStep {
+  None,
+  Half,
+  Full,
+}
+
+const baseBorderStle = {
+  backgroundColor: 'hsla(212, 50%, 50%, 0)',
+  bottom: rem(8),
+  height: '4px',
+  left: 0,
+  position: 'absolute',
+  transform: 'scaleX(0)',
+  width: '100%',
+};
+
+const halfBorderStyle = {
+  ...baseBorderStle,
+  backgroundColor: 'hsla(212, 50%, 50%, 1.0)',
+  transform: 'scaleX(1)',
+};
+
+const fullBorderStyle = {
+  ...baseBorderStle,
+  backgroundColor: 'hsla(90, 50%, 50%, 1.0)',
+  transform: 'scaleX(1)',
+};
+
+function ReactiveBorder(props: { step: BorderStep }) {
+  const { step } = props;
+
+  return (
+    <>
+      <Spring
+        native
+        to={step !== BorderStep.None ? halfBorderStyle : baseBorderStle}
+        render={styles => <animated.span style={styles} />}
+      />
+      <Spring
+        native
+        to={step === BorderStep.Full ? fullBorderStyle : baseBorderStle}
+        render={styles => <animated.span style={styles} />}
+      />
+    </>
+  );
+}
+
+function getBorderStep(options: { focus: boolean; hover: boolean }) {
+  const { focus, hover } = options;
+  if (focus) {
+    return BorderStep.Full;
   }
+  if (hover) {
+    return BorderStep.Half;
+  }
+  return BorderStep.None;
+}
 
-  return baseStyles;
+interface Props extends FieldProps<any> {
+  className?: string;
+  label: string;
+  type?: HTMLProps<HTMLInputElement>['type'];
 }
 
 function Input({ className, field, label, form, ...props }: Props) {
   const touched = form.touched[field.name];
   const error = form.errors[field.name];
+  const id = `field-${field.name}`;
 
   return (
     <Toggle>
@@ -103,27 +163,24 @@ function Input({ className, field, label, form, ...props }: Props) {
               onMouseEnter={hover.setOn}
               onMouseLeave={hover.setOff}
             >
-              <Spring
-                native
-                to={field.value === '' ? PLACEHOLDER : LABEL}
-                render={styles => (
-                  <animated.label style={styles}>{label}</animated.label>
-                )}
+              <InputField
+                {...props}
+                {...field}
+                id={id}
+                placeholder={undefined}
+                onFocus={focus.setOn}
+                onBlur={focus.setOff}
               />
-              <InputWrapper>
-                <Spring
-                  native
-                  render={styles => <animated.span style={styles} />}
-                  to={getHoverStyles({ focus: focus.on, hover: hover.on })}
-                />
-                <InputField
-                  {...props}
-                  {...field}
-                  placeholder={undefined}
-                  onFocus={focus.setOn}
-                  onBlur={focus.setOff}
-                />
-              </InputWrapper>
+              <ReactiveLabel
+                htmlFor={id}
+                focused={focus.on}
+                empty={field.value === ''}
+              >
+                {label}
+              </ReactiveLabel>
+              <ReactiveBorder
+                step={getBorderStep({ focus: focus.on, hover: hover.on })}
+              />
               {touched && error && <Notice>{error}</Notice>}
             </InputContainer>
           )}
