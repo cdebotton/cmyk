582521ca252015d7dded62b75bf73b7480c49342
Christian de Botton <cdebotton@gmail.com>
2018-10-22 22:00:03 -0400

    Use css transforms instead of svg

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -12,7 +12,8 @@ interface Props extends FieldProps<any> {
 }
 
 const InputField = styled.input`
-  padding: ${rem(6)};
+  position: relative;
+  padding: ${rem(8)};
   height: ${rem(32)};
   width: 100%;
   border: none;
@@ -21,6 +22,14 @@ const InputField = styled.input`
   &:focus {
     outline: none;
   }
+`;
+
+const InputWrapper = styled.span`
+  position: relative;
+  overflow: hidden;
+  display: block;
+  border-radius: 3px;
+  background-color: hsla(0, 0%, 100%, 0.2);
 `;
 
 const Notice = styled.span`
@@ -36,63 +45,48 @@ const InputContainer = styled.span`
   padding: ${rem(16)} 0 ${rem(8)};
 `;
 
-const StateFiller = styled.svg`
-  z-index: -1;
-  pointer-events: none;
-  border-radius: 3px;
-  ${position('absolute', 0, 0)};
-  ${size('100%')};
-`;
-
-const NativeStyles: { [x: string]: CSSProperties } = {
-  label: {
-    left: rem(8),
-    pointerEvents: 'none',
-    position: 'absolute',
-    top: 0,
-  },
+const labelStyles: CSSProperties = {
+  left: rem(8),
+  pointerEvents: 'none',
+  position: 'absolute',
+  top: 0,
 };
 
-const PLACEHOLDER = {
-  ...NativeStyles.label,
+const PLACEHOLDER: CSSProperties = {
+  ...labelStyles,
   color: '#eee',
   fontSize: modularScale(0),
   transform: `translate3d(0, ${rem(24)}, 0)`,
 };
 
-const LABEL = {
-  ...NativeStyles.label,
+const LABEL: CSSProperties = {
+  ...labelStyles,
   color: '#fff',
   fontSize: modularScale(-1),
   transform: `translate3d(0, ${rem(3)}, 0)`,
 };
 
-const PathStates = {
-  empty: 'M0,16 L0,16 L0,48 L0,48 Z',
-  full: 'M0,16 L100,16 L100,48 L0,48 Z',
-  partial: 'M0,16 L80,16 L90,48 L0,48 Z',
-};
-
-function getHoverSpringTo(options: { hover: boolean; focus: boolean }) {
-  const { hover, focus } = options;
-
-  if (focus) {
-    return { d: PathStates.full, fill: 'hsla(212, 50%, 50%, 0.5)' };
-  }
+function getHoverStyles(options: { hover: boolean; focus: boolean }) {
+  const { hover } = options;
+  const baseStyles: CSSProperties = {
+    backgroundColor: 'hsla(212, 50%, 50%, 0)',
+    height: '100%',
+    left: 0,
+    position: 'absolute',
+    top: 0,
+    transform: `translate3d(100%, 0, 0) skew(-30deg)`,
+    width: 'calc(100%)',
+  };
 
   if (hover) {
-    return { d: PathStates.partial, fill: 'hsla(212, 50%, 50%, 0.5)' };
+    return {
+      ...baseStyles,
+      backgroundColor: 'hsla(240, 50%, 50%, 0.5)',
+      transform: `translate3d(16px, 0, 0) skew(45deg)`,
+    };
   }
 
-  return { d: PathStates.empty, fill: 'hsla(180, 50%, 50%, 0.0)' };
-}
-
-function getFocusSpringTo(focus: boolean) {
-  if (focus) {
-    return { d: PathStates.full, fill: 'hsla(250, 50%, 50%, 0.5)' };
-  }
-
-  return { d: PathStates.empty, fill: 'hsla(180, 50%, 50%, 0.0)' };
+  return baseStyles;
 }
 
 function Input({ className, field, label, form, ...props }: Props) {
@@ -109,33 +103,27 @@ function Input({ className, field, label, form, ...props }: Props) {
               onMouseEnter={hover.setOn}
               onMouseLeave={hover.setOff}
             >
-              <Spring native to={field.value === '' ? PLACEHOLDER : LABEL}>
-                {styles => (
+              <Spring
+                native
+                to={field.value === '' ? PLACEHOLDER : LABEL}
+                render={styles => (
                   <animated.label style={styles}>{label}</animated.label>
                 )}
-              </Spring>
-              <StateFiller viewBox="0 0 100 56" preserveAspectRatio="none">
-                <path d={PathStates.full} fill="hsla(0, 0%, 100%, 0.2)" />
-                <Spring<{}, ReturnType<typeof getHoverSpringTo>>
-                  native
-                  to={getHoverSpringTo({ hover: hover.on, focus: focus.on })}
-                >
-                  {({ d, fill }) => <animated.path d={d} fill={fill} />}
-                </Spring>
-                <Spring<{}, ReturnType<typeof getFocusSpringTo>>
-                  native
-                  to={getFocusSpringTo(focus.on)}
-                >
-                  {({ d, fill }) => <animated.path d={d} fill={fill} />}
-                </Spring>
-              </StateFiller>
-              <InputField
-                {...props}
-                {...field}
-                placeholder={undefined}
-                onFocus={focus.setOn}
-                onBlur={focus.setOff}
               />
+              <InputWrapper>
+                <Spring
+                  native
+                  render={styles => <animated.span style={styles} />}
+                  to={getHoverStyles({ focus: focus.on, hover: hover.on })}
+                />
+                <InputField
+                  {...props}
+                  {...field}
+                  placeholder={undefined}
+                  onFocus={focus.setOn}
+                  onBlur={focus.setOff}
+                />
+              </InputWrapper>
               {touched && error && <Notice>{error}</Notice>}
             </InputContainer>
           )}
