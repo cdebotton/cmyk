fad4ec2c46050af1496bb7006e65be7955380f4c
Christian de Botton <cdebotton@gmail.com>
2018-10-23 22:49:20 -0400

    Begin select

--- src/app/components/Select.tsx
+++ src/app/components/Select.tsx
@@ -9,31 +9,71 @@ interface Option<T> {
 
 interface Props<T> extends FieldProps<any> {
   className?: string;
-  value: T;
+  label: ReactNode;
   options: Option<T>[];
 }
+
+const SelectContainer = styled.div``;
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const SelectLabel = styled.div``;
+
+const OptionList = styled.ul`
+  position: absolute;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  top: 0;
+`;
+
+const Option = styled.li`
+  cursor: pointer;
+`;
 
 function Select<T extends string>({
   className,
   options,
-  value,
   form,
   field,
   ...props
 }: Props<T>) {
+  const selected = options
+    .map<[ReactNode, T]>(({ label, value: optionValue }) => [
+      label,
+      optionValue,
+    ])
+    .find(([_, optionValue]) => optionValue === field.value);
+
+  let selectedLabel: ReactNode;
+  if (!selected) {
+    selectedLabel = props.label;
+  } else {
+    [selectedLabel] = selected;
+  }
+
   return (
     <SelectContainer className={className}>
-      <select {...props} {...field}>
-        {options.map(option => (
-          <option key={`OPTION_${option.value}`} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+      <SelectWrapper>
+        <SelectLabel>{selectedLabel}</SelectLabel>
+        <OptionList>
+          {options.map(option => (
+            <Option
+              key={`OPTION_${field.name}_${option.value}`}
+              onClick={event => {
+                form.setFieldValue(field.name, option.value);
+              }}
+            >
+              {option.label}
+            </Option>
+          ))}
+        </OptionList>
+      </SelectWrapper>
     </SelectContainer>
   );
 }
-
-const SelectContainer = styled.span``;
 
 export default Select;
