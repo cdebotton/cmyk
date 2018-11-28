349466e9c69991cfdf79a4e1d827b412e58d9679
Christian de Botton <cdebotton@gmail.com>
2018-11-28 01:37:02 -0500

    Create document migration

--- db/migrations/1543385667989-add-document.ts
+++ db/migrations/1543385667989-add-document.ts
@@ -4,7 +4,19 @@ export async function up() {
   const pool = new Pool();
 
   await pool.query(`
-    -- Your up query
+    CREATE TABLE cmyk.document (
+      id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      title         TEXT NOT NULL,
+      published_at  TIMESTAMP DEFAULT now(),
+      created_at    TIMESTAMP DEFAULT now(),
+      updated_at    TIMESTAMP DEFAULT now(),
+      author_id     UUID REFERENCES cmyk.user(id) ON DELETE SET NULL,
+    );
+
+    CREATE TRIGGER file_updated_at BEFORE UPDATE
+    ON cmyk.file
+    FOR EACH ROW
+    EXECUTE PROCEDURE cmyk_private.set_updated_at();
   `);
 
   await pool.end();
@@ -14,7 +26,8 @@ export async function down() {
   const pool = new Pool();
 
   await pool.query(`
-    -- Your down query
+    DROP TRIGGER file_updated_at ON cmyk.file;
+    DROP TABLE cmyk.document;
   `);
 
   await pool.end();
